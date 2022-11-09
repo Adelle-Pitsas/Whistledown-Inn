aa0967b881a5ac53308b0cc616faa6b32a46a5e2
Adelle Pitsas <apitsas4@gmail.com>
2022-11-09 16:39:16 -0500

    add customer class and tests

--- src/CustomerRepository.js
+++ src/CustomerRepository.js
@@ -1,13 +1,23 @@
+import Customer from "./Customer";
+
 class CustomerRepository {
   constructor(allCustomerData) {
-    this.allCustomers = allCustomerData || [];
+    this.allCustomers = this.makeAllCustomers(allCustomerData) || [];
   }
 
+  makeAllCustomers(data) {
+    return data.map((data) => {
+      return new Customer(data.id, data.name)
+    })
+  }
+  
   findCustomerByID(id) {
     return this.allCustomers.find((customer) => {
       return customer.id === id
     })
   }
+
+
 }
 
 export default CustomerRepository

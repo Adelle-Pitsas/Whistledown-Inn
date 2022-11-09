0552c83fd4db2b2c9340515beab7fe3e9b614cf9
Adelle Pitsas <apitsas4@gmail.com>
2022-11-09 17:28:24 -0500

    Add room class and tests

---
+++ src/data/Rooms-data.js
@@ -0,0 +1,36 @@
+const rooms = [
+  {
+    number: 12,
+    roomType: "single room",
+    bidet: false,
+    bedSize: "twin",
+    numBeds: 2,
+    costPerNight: 172.09
+    },
+    {
+      number: 18,
+      roomType: "junior suite",
+      bidet: false,
+      bedSize: "king",
+      numBeds: 2,
+      costPerNight: 496.41
+    },
+    {
+      number: 9,
+      roomType: "single room",
+      bidet: true,
+      bedSize: "queen",
+      numBeds: 1,
+      costPerNight: 200.39
+    },
+    {
+      number: 20,
+      roomType: "residential suite",
+      bidet: false,
+      bedSize: "queen",
+      numBeds: 1,
+      costPerNight: 343.95
+    }
+]
+
+export default rooms

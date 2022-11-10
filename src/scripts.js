51a6b59786bbcf6e21e073bb7d4ba996f6dfe423
Adelle Pitsas <apitsas4@gmail.com>
2022-11-10 14:37:27 -0500

    add initialzeApp function

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,42 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import CustomerRepository from './CustomerRepository';
+import Hotel from './Hotel';
+import getAllData from './Network-requests';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+// ------GLOBAL VARIABLES------
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const store = {
+  hotelData: new Hotel(),
+  customerRepo: new CustomerRepository(),
+  bookingData: [],
+  roomData: [],
+  customersData: []
+}
+
+// -------WINDOW LOAD FUNCTIONS------
+function initializeApp() {
+  getAllData()
+    .then((data) => {
+      store.roomData = data.roomsData
+      store.bookingData = data.bookingsData;
+      store.hotelData = new Hotel(store.roomData, store.bookingData)
+      store.customersData = data.customersData
+      store.customerRepo = new CustomerRepository(store.customersData)
+      doTheThing()
+    })
+}
+
+
+// ------EVENT LISTENERS------
+window.addEventListener('load', initializeApp)
+
+
+// ------EVENT HANDLERS------
+
+
+// ------UTILITY FUNCTIONS------

efb5257cdefb7d9e6b9c9b3e889d329a5fb556d0
Adelle Pitsas <apitsas4@gmail.com>
2022-11-09 19:23:44 -0500

    add find availability and filter by room methods and tests

--- src/Hotel.js
+++ src/Hotel.js
@@ -18,6 +18,26 @@ class Hotel {
       return new Booking(booking)
     })
   }
+
+  getAvailableRooms(date) {
+    const notAvailableRooms = this.allBookings
+      .filter((booking) => {
+       return booking.date === date;
+      })
+      .map((booking) => {
+        return booking.roomNumber
+      })
+    return this.allRooms.filter((room) => {
+      return !notAvailableRooms.includes(room.number)
+    })
+  }
+
+  filterByRoomType(date, roomType) {
+    const availableRooms = this.getAvailableRooms(date);
+    return availableRooms.filter((room) => {
+      return room.roomType === roomType
+    })
+  }
 }
 
 export default Hotel
--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -7,14 +7,16 @@ import Booking from '../src/Booking'
 const expect = chai.expect;
 
 describe('Hotel', () => {
-let room1, room2, booking1, booking2, roomsData, bookingsData, newHotel;
+let room1, room2, room3, room4, booking1, booking2, roomsData, bookingsData, newHotel;
 
 beforeEach(() => {
   room1 = rooms[0]
   room2 = rooms[1]
+  room3 = rooms[2]
+  room4 = rooms[3]
   booking1 = bookings[0]
   booking2 = bookings[1]
-  roomsData = [room1, room2]
+  roomsData = [room1, room2, room3, room4]
   bookingsData = [booking1, booking2]
   newHotel = new Hotel(roomsData, bookingsData)
 })
@@ -53,5 +55,47 @@ beforeEach(() => {
 
   it('should contain a list of bookings that are instances of Booking', () => {
     expect(newHotel.allBookings[0]).to.be.an.instanceOf(Booking)
+  })
+
+  it('should be able to get available rooms depending on the date', () => {
+    const availableRooms = newHotel.getAvailableRooms("2022/02/05");
+    expect(availableRooms).to.deep.equal([
+      {
+        number: 18,
+        roomType: "junior suite",
+        bidet: false,
+        bedSize: "king",
+        numBeds: 2,
+        costPerNight: 496.41
+      },
+      {
+        number: 9,
+        roomType: "single room",
+        bidet: true,
+        bedSize: "queen",
+        numBeds: 1,
+        costPerNight: 200.39
+      },
+      {
+        number: 20,
+        roomType: "residential suite",
+        bidet: false,
+        bedSize: "queen",
+        numBeds: 1,
+        costPerNight: 343.95
+      }
+    ])
+  })
+
+  it('should be able to filter available rooms by room type', () => {
+    const filteredRooms = newHotel.filterByRoomType("2022/02/05","single room")
+      expect(filteredRooms).to.deep.equal([{
+        number: 9,
+        roomType: "single room",
+        bidet: true,
+        bedSize: "queen",
+        numBeds: 1,
+        costPerNight: 200.39
+      }])
   })
 });

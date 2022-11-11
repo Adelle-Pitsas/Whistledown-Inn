3fead205c5061a95f7c4c7ef1c160e8376cfb3fd
Adelle Pitsas <apitsas4@gmail.com>
2022-11-10 19:03:56 -0500

    add search room functionality

--- src/scripts.js
+++ src/scripts.js
@@ -16,6 +16,12 @@ const bookingDropDown = document.getElementById('bookingDropDown')
 const bookings = document.getElementById('bookings')
 const dropDownArrow = document.querySelector('.dropdown-arrow')
 const bookingContainer = document.getElementById('dropdownContainer')
+const datePicker = document.getElementById('datePicker')
+const roomTypePicker = document.getElementById('roomTypeSelect')
+const submitButton = document.getElementById('submitSearchButton')
+const chooseDateError = document.getElementById('chooseDateError')
+const availableRoomsContainer = document.getElementById('availableRoomsContainer')
+
 
 
 // ------GLOBAL VARIABLES------
@@ -49,6 +55,8 @@ window.addEventListener('load', initializeApp)
 function initializeEventListeners() {
   
   bookingDropDown.addEventListener('click', toggleBookingsDisplay)
+
+  submitButton.addEventListener('click', searchFilter)
 }
 
 
@@ -93,8 +101,47 @@ function displayCustomerBookings() {
   })
 }
 
+function searchFilter() {
+  if(datePicker.value && roomTypePicker.value==='default-select') {
+    displayAvailableRooms(store.hotel.getAvailableRooms(datePicker.value))
+  } else if(datePicker.value && roomTypePicker !== 'default-select') {
+    displayAvailableRooms(store.hotel.filterByRoomType(datePicker.value, roomTypePicker.value))
+  } else {
+    show(chooseDateError)
+  }
+}
+
+function displayAvailableRooms(rooms) {
+  availableRoomsContainer.innerHTML = ''
+  rooms.forEach((room) => {
+    availableRoomsContainer.innerHTML+= `
+      <section class="room-card" id="roomCard">
+        <figure class="picture">
+          <img src="bedroomImage.png" class="bedroom-image" alt="victorian bedroom">
+        </figure>
+        <section class="room-details">
+          <p class="room-number">Room Number: ${room.number}</p>
+          <p class="room-type">${room.roomType}</p>
+          <p class="bed-size">${room.bedSize}</p>
+          <p class="examplenumber-of-beds">Number of beds: ${room.numBeds}</p>
+          <p class="example-cost-per-night">$${room.costPerNight}</p>
+        </section>
+        <button class="book-room-button">BOOK ROOM</button>
+      </section>
+    `
+  })  
+}
+
 
 // ------UTILITY FUNCTIONS------
 function getCustomer() {
   return store.customerRepo.findCustomerByID(1)
+}
+
+function hide(element) {
+  element.classList.add('hidden')
+}
+
+function show(element) {
+  element.classList.remove('hidden')
 }
